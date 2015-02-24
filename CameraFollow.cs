f3208e11590fa8c3035ea450468a68ad57f56187
HugePedlar <github@mail.hugepedlar.com>
2015-02-24 17:18:13 +1030

    Initial commit. 2D camera scripts.

---
+++ CameraFollow.cs
@@ -0,0 +1,52 @@
+﻿// FREE FOR ALL USES
+// 2D only
+// This script requires the CameraShake script to be on the same gameObject.
+// It also requires a separate GameObject tagged "Player" with a rigidBody2D.
+
+// 'damping' controls the smoothness of the camera movement.
+// 'leading' controls how far ahead of the target's trajectory the camera aims for.
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float damping = 2.0f;
+    public float leading = 0.4f;
+
+    private Vector3 oldTargetPosition;
+    private Vector3 currentPosition;
+    private Vector3 originalPosition;
+    private Vector3 wantedPosition;
+
+    private CameraShake cameraShake;
+
+    void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        oldTargetPosition = target.position;
+        originalPosition = transform.position;
+
+        cameraShake = GetComponent<CameraShake>();
+    }
+
+    void LateUpdate()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        
+        wantedPosition = target.position + (Vector3)target.rigidbody2D.velocity * leading;
+        currentPosition = transform.position;
+        currentPosition = Vector3.Lerp(currentPosition, wantedPosition, damping * Time.deltaTime);
+        currentPosition.z = originalPosition.z;
+        transform.position = currentPosition;
+        cameraShake.originalCamPos = currentPosition;
+    }
+}
---
+++ CameraShake.cs
@@ -0,0 +1,53 @@
+﻿// FREE FOR ALL USES
+// 2D only
+// Put this on a camera along with the CameraFollow script.
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+    [Header("Defaults")]
+    public float duration = 0.5f;
+    public float magnitude = 1.0f;
+
+    [HideInInspector]
+    public Vector3 originalCamPos;
+
+    public void Shake()
+    {
+        StopCoroutine("_Shake");
+        StartCoroutine(_Shake(duration, magnitude));
+    }
+
+    public void Shake(float _duration, float _magnitude)
+    {
+        StopCoroutine("_Shake");
+        StartCoroutine(_Shake(_duration, _magnitude));
+    }
+
+    IEnumerator _Shake(float _duration, float _magnitude)
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < _duration)
+        {
+            elapsed += Time.deltaTime;
+
+            float percentComplete = elapsed / _duration;
+            float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+            // map value to [-1, 1]
+            float x = Random.value * 2.0f - 1.0f;
+            float y = Random.value * 2.0f - 1.0f;
+            x *= _magnitude * damper;
+            y *= _magnitude * damper;
+
+            transform.position = new Vector3(x + originalCamPos.x, y + originalCamPos.y, originalCamPos.z);
+
+            yield return null;
+        }
+
+        transform.position = originalCamPos;
+    }
+}
